227f5cb67d75d653a4bdd9a74a050fb5524c4c91
asoeb <a.soeborg-skau@hotmail.dk>
2026-01-29 15:28:22 +0100

    gruppenavn

--- repositories.py
+++ repositories.py
@@ -10,3 +10,16 @@ GROUP_REPOS = [
     "sla": "",
 }
 ]
+
+GROUP_REPOS = [
+{
+    "name": "ASCDevOps",
+    "gitLinks": ["https://github.com/ASCDevOps/whoknows_variations"],
+    "backend": "",
+    "frontend": "",
+    "monitoring": "",
+    "stack": [],
+    "documentation": [],
+    "sla": "",
+}
+]
