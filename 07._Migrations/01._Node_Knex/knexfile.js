82b5050240f70b87aed39526b6fe029c44c79276
Anders Latif <andl@ek.dk>
2026-04-09 13:10:00 +0200

    Set up the Postgresql database

---
+++ 07._Migrations/01._Node_Knex/knexfile.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
+
+/**
+ * @type { import("knex").Knex.Config }
+ */
+export default {
+  client: 'postgresql',
+  connection: {
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
