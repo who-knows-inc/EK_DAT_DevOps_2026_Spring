e486da6fe9b52a66167a47f20f3f6f81627ce055
Anders Latif <andl@ek.dk>
2026-04-14 14:18:56 +0200

    Improved the Scrapy slides

--- 09._Scrapy/WikiCrawler/WikiCrawler/spiders/WikiPageSpider.py
+++ 09._Scrapy/WikiCrawler/WikiCrawler/spiders/WikiPageSpider.py
@@ -6,8 +6,21 @@ class WikipageSpider(CrawlSpider):
     name = 'WikiPageSpider'
     allowed_domains = ['en.wikipedia.org']
     start_urls = ['https://en.wikipedia.org/wiki/List_of_common_misconceptions']
+    custom_settings = {
+        'CONCURRENT_REQUESTS_PER_DOMAIN': 1,  # Wikipedia asks for this
+        'DOWNLOAD_DELAY': 2,  # Politeness
+        'AUTOTHROTTLE_ENABLED': True,
+        'AUTOTHROTTLE_START_DELAY': 1,
+        'AUTOTHROTTLE_MAX_DELAY': 10,
+        'AUTOTHROTTLE_TARGET_CONCURRENCY': 1,
+        'RANDOMIZE_DOWNLOAD_DELAY': True,
+        'USER_AGENT': 'Mozilla/5.0 (compatible; MyWikiBotv1.0)',  # Wikipedia requires a User-Agent
+        'ROBOTSTXT_OBEY': True,
+        'LOG_LEVEL': 'INFO'  # DEBUG is verbose, INFO is cleaner
+    }
+    
     rules = (
-        Rule(LinkExtractor(allow=r'/wiki/'), callback='parse_item', follow=True, process_links='process_links'),
+        Rule(LinkExtractor(allow=r'/wiki/'), callback='parse_item', follow=False),
     )
     visited_urls = set()
 
@@ -17,10 +30,7 @@ class WikipageSpider(CrawlSpider):
         self.visited_urls.add(response.url)
         yield {
             'url': response.url,
-            'title': response.css('h1::text').get(),
-            'content': response.css('p::text').getall()
+            'title': response.css('h1 span.mw-page-title-main::text').get(),
+            'content': response.css('#mw-content-text p::text').getall()
         }
         
-    def process_links(self, links):
-        # Filter out already visited URLs
-        return [link for link in links if link.url not in self.visited_urls]
